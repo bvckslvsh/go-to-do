4a659641c557be1548a61ed7e9c46867710f553c
Dima Kozyrev <bvckslvsh@gmail.com>
2023-04-14 13:29:23 +0300

    db connected

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/bvckslvsh/go-to-do/pkg/handler"
 	"github.com/bvckslvsh/go-to-do/pkg/repository"
 	"github.com/bvckslvsh/go-to-do/pkg/service"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
 
@@ -14,7 +15,20 @@ func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("error while initializing config files: %s", err.Error())
 	}
-	repos := repository.NewRepository()
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: viper.GetString("db.password"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
